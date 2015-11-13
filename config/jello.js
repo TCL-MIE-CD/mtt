df004e4a66fff9ec8c95e11180dd189fe860e754
mmillet <1007650009@qq.com>
2015-11-13 11:17:11 +0800

    删掉jello 配置

--- config/jello.js
+++ config/jello.js
@@ -3,6 +3,7 @@
  * Created by zhengguo.chen on 2015/11/10.
  */
 var exports = module.exports = function(fis) {
-  console.log("--------MTT JELLO--------");
+  //do nothing
+  //该配置独立到mtt-jello中了
 };
 exports.init = exports;
